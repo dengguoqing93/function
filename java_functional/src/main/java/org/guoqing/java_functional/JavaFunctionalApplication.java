eb3706e313c1e6bec3e123ec4ccb80b1d72d787e
dengguoqing <dengguoqing93@163.com>
2018-10-07 09:32:47 +0800

    Initial commit

---
+++ java_functional/src/main/java/org/guoqing/java_functional/JavaFunctionalApplication.java
@@ -0,0 +1,12 @@
+package org.guoqing.java_functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFunctionalApplication.class, args);
+	}
+}
---
+++ java_functional/src/test/java/org/guoqing/java_functional/JavaFunctionalApplicationTests.java
@@ -0,0 +1,16 @@
+package org.guoqing.java_functional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaFunctionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
